87d5b54186a0706c9ea1cf0708960292c7882209
mintcc342g <mintcc342g@gmail.com>
2020-08-16 16:20:17 +0900

    fix typo

--- swagger_study/urls.py
+++ swagger_study/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
-
 # drf-yasg
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -29,27 +28,26 @@ schema_view = get_schema_view(
         default_version="v1",
         description="Swagger Study를 위한 API 문서",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(
-            name="test",
-            email="test@test.com",
-        ),
-        license=openapi.License(
-            name="Test License",
-        ),
-        permission_classes=(permissions.AllowAny,),
-        public=True,
-    )
+        contact=openapi.Contact(name="test", email="test@test.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(("api.urls", "api"))),  # 편의를 위해(?) api 앱의 세부 url은 api앱 내부의 urls.py 파일에서 관리할 것 
+    # 편의를 위해(?) api 앱의 세부 url은 api앱 내부의 urls.py 파일에서 관리할 것
+    path("api/", include(('api.urls', 'api'))),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+                schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger',
+                                                   cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc',
+                                                 cache_timeout=0), name='schema-redoc'),
     ]
