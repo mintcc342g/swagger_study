d03d71df402b9a97135046997af2c6b32872ae07
mintcc342g <mintcc342g@gmail.com>
2020-08-16 16:31:12 +0900

    edit views, add serializers

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from .models import PlayList
+from rest_framework import serializers
+
+class PlayListSerializer(serializers.ModelSerializer):
+    class Meta:
+        play_list = PlayList.objects.all()
+        model = PlayList
+        fields = '__all__'  # __all__ 을 줄 경우, 모든 필드가 사용됨.
+        # fields = ('id', 'created_at', 'title', 'category', 'star_rating',)  # 필드(컬럼)를 직접 명시하면, 명시된 필드만 사용됨.
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status, viewsets, mixins
 from rest_framework.response import Response
 from django.views import View
-from .models import PlayList
 
+from .models import PlayList
+from .serializers import PlayListSerializer
 # Create your views here.
 
 
@@ -10,7 +11,7 @@ class ApiViewSet(viewsets.GenericViewSet,
                 mixins.ListModelMixin,
                 View):
     
-    serializer_class = None
+    serializer_class = PlayListSerializer
 
     def get_queryset(self):
         music_num = self.kwargs.get("music_num", None)
@@ -20,3 +21,17 @@ class ApiViewSet(viewsets.GenericViewSet,
             play_list = PlayList.objects.filter(id=music_num)
         
         return play_list    # get_queryset은 list 형태로 반환해줘야 알아서 시리얼라이즈 먹여서 내보내줌.
+    
+    def add(self, request):
+        play_list = PlayList.objects.filter(**request.data)
+        if play_list.exists():
+            return Response(data={'result': 'the music already exists.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        # PlayList.objects.create() 를 사용해도 되겠지만, 시리얼라이저가 있으니 시리얼라이저로 해보자.
+        play_list_serializer = PlayListSerializer(data=request.data, partial=True)
+        if not play_list_serializer.is_valid():
+            return Response(data={'result': 'the input data is invalid.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        play_list = play_list_serializer.save()
+
+        return Response(PlayListSerializer(play_list).data, status=status.HTTP_201_CREATED)
