8fa92c85e45954c4d276204ce83c601551b48ebb
mintcc342g <mintcc342g@gmail.com>
2020-08-16 16:20:56 +0900

    add a field in the model

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class PlayList(models.Model):
     ONE_STAR = 1
     TWO_STAR = 2
@@ -22,15 +23,19 @@ class PlayList(models.Model):
     id = models.BigAutoField(primary_key=True, verbose_name='pk')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='추가된 날짜')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='업뎃 된 날짜')
-    deleted_at = models.DateTimeField(blank=True, null=True, verbose_name='삭제된 날짜')
+    deleted_at = models.DateTimeField(
+        blank=True, null=True, verbose_name='삭제된 날짜')
+    singer = models.CharField(null=False, max_length=128, verbose_name='가수명')
     title = models.CharField(null=False, max_length=128, verbose_name='곡명')
-    category = models.CharField(blank=True, null=True, max_length=32, choices=CATEGORY, verbose_name='범주')
-    star_rating = models.PositiveSmallIntegerField(blank=True, null=True, choices=STARS, default=ONE_STAR, verbose_name='곡 선호도')
+    category = models.CharField(
+        blank=True, null=True, max_length=32, choices=CATEGORY, verbose_name='범주')
+    star_rating = models.PositiveSmallIntegerField(
+        blank=True, null=True, choices=STARS, default=ONE_STAR, verbose_name='곡 선호도')
 
     class Meta:
         # abstract = True
         managed = True
         db_table = 'play_lists'
         app_label = 'api'
-        ordering = ['star_rating',]
+        ordering = ['star_rating', ]
         verbose_name_plural = '나의 플레이 리스트'
